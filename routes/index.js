1d16b9b32d3015ff6f17352a3a40477574027193
Piepongwong <j.tonneyck@gmail.com>
2018-12-05 15:40:27 +0100

    running

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/countries/:country', (req, res, next) => {
+  var countries = require("./countries.json")
+  
+  var countriesFiltered = countries.filter((country)=> 
+    country.name.common.toLowerCase().includes(req.params.country.toLowerCase()) ||  country.name.official.includes(req.params.country.toLowerCase())
+
+  )
+
+  res.json(countriesFiltered);
+});
+
+module.exports = router;
