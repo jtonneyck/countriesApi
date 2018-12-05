eca4626feb7c253c12a026b4de817c964e46f69e
Piepongwong <j.tonneyck@gmail.com>
2018-12-05 17:53:04 +0100

    also all countries

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,20 @@ var router = express.Router();
 router.get('/countries/:country', (req, res, next) => {
   var countries = require("./countries.json")
   
+  var searchCountry = req.params.country.toLowerCase()
   var countriesFiltered = countries.filter((country)=> 
-    country.name.common.toLowerCase().includes(req.params.country.toLowerCase()) ||  country.name.official.includes(req.params.country.toLowerCase())
-
+    country.name.common.toLowerCase().includes(searchCountry) || country.name.official.includes(searchCountry)
   )
+  
   res.setHeader('Content-Type', 'application/json');
   res.send(countriesFiltered);
+});
+
+router.get('/countries', (req, res, next) => {
+  var countries = require("./countries.json")
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(countries);
 });
 
 module.exports = router;
